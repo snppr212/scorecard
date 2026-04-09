c4e4d7c9515f51f82001ed8110cdbe73812acb30
snppr212 <samuel.thomas86@gmail.com>
2026-04-09 16:32:43 -0400

    v14: fix pitch sequence display, dedup popups, modal for 75p, HBP routing, override button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mm-v13';
+const CACHE = 'mm-v14';
 const FILES = ['./index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
