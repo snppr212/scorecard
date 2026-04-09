663f5e944e7696d44a3952c18537204d16df6edc
snppr212 <samuel.thomas86@gmail.com>
2026-04-09 16:18:54 -0400

    v13: chkPW in finAB covers all AB outcomes (hits, outs, everything)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mm-v12';
+const CACHE = 'mm-v13';
 const FILES = ['./index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
