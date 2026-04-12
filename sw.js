ff4641f9e956a3fcd0af642d8183ab35e7aa8e9b
snppr212 <samuel.thomas86@gmail.com>
2026-04-11 21:19:34 -0400

    Add Jack Van Dyke to Hot Rods opp roster (v33)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mm-v32';
+const CACHE = 'mm-v33';
 const FILES = ['./index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
